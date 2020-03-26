1286c25c4971da9032ce5d12cb0287f23e5cd2ae
Richard Kempert <ritchyk@richards-mbp.home>
2020-03-26 16:03:11 +0100

    first & last name

--- javasricpt/hello.js
+++ javasricpt/hello.js
@@ -7,3 +7,13 @@
 //alert (message);
 
 const LINK_COLOR = "#ff0000";
+console.log ("Link bitte in der Farbe ", LINK_COLOR)
+
+
+let highscore = 520233
+console.log (highscore/ 10)
+
+let firstname ="Bob"
+let lastname ="Jooo"
+
+console.log ('Name: ', firstname, lastname)
