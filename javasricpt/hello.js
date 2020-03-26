67b842f9efb5145331a4ef1f9ed7c81f49b27b05
Richard Kempert <ritchyk@richards-mbp.home>
2020-03-26 16:24:13 +0100

    user_gen

--- javasricpt/hello.js
+++ javasricpt/hello.js
@@ -7,16 +7,39 @@
 //alert (message);
 
 const LINK_COLOR = "#ff0000";
-console.log ("Link bitte in der Farbe ", LINK_COLOR)
+console.log("Link bitte in der Farbe ", LINK_COLOR)
 
 
 let highscore = 520233
-console.log (highscore/ 10)
+console.log(highscore / 10)
 
-let firstname ="Bob"
-let lastname ="Jooo"
+let firstname = "Bob"
+let lastname = "Jooo"
 
-console.log ('Name: ', firstname, lastname)
+console.log('Name: ', firstname, lastname);
 
-let template =  'Dein Highscore sind ${highscore} Punkte';
-console.log (template);
+let template = `Dein Highscore sind ${highscore} Punkte`;
+console.log(template);
+
+let isOver18 = "true";
+console.log(isOver18)
+
+let age = 18;
+console.log("über 18", age > 18);
+
+let participants = ("John", "Jane", "Max");
+console.log(participants[3])
+console.log("eingräge im Arrey: ", participants.length)
+
+let gameHighscores = [2099, 3010, 3333, 5000]
+console.log(gameHighscores)
+
+let user = {
+    firstname: "JOhn",
+    lastname: "Bob",
+    age: 25
+};
+
+console.log(user)
+user.highscore = 200;
+console.log (user)
