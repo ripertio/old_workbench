78bc735cb899cc43bab7cd2b269cccee3d930a07
Richard Kempert <ritchyk@richards-mbp.home>
2020-03-27 13:40:31 +0100

    first try adding map to page

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,8 @@
 //alter ("Hallo Welt")
+
+var mapNapier = L.map('map').setView([-39.490556,176.904444], 10);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+})
