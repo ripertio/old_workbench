f5852d6591fef15e47c47f79e9311d0003b80a97
Richard Kempert <ritchyk@richards-mbp.home>
2020-04-02 16:11:48 +0200

    änderung nach Vorgehen in der VOrlesung

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,5 @@
 //alter ("Hallo Welt")
-
+/*
 var mapNapier = L.map('map').setView([-39.490556, 176.904444], 13);
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
@@ -7,3 +7,23 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 }).addTo(mapNapier)
 var markerNapier = L.marker([-39.490556, 176.904444]).addTo(mapNapier)
 markerNapier.bindPopup("Napier-Stadt").openPopup();
+
+*/
+
+let mapdiv = document.querySelector("#map");
+let map = L.map("map", {
+            center: [
+                mapdiv.dataset.lat,
+                mapdiv.dataset.lng
+            ],
+            zoom: 13,
+            layers: [
+                L.tileLayer.provider('OpenTopoMap')
+            ]
+        });
+let mrk = L.marker([
+    mapdiv.dataset.lat,
+    mapdiv.dataset.lng
+]).addTo(map);
+
+mrk.bindPopup(mapdiv.dataset.title).openPopup();
