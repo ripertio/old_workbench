eb9047ea65a5d00c0e7e4e2ca5c8e8f1f791d0cf
Richard Kempert <ritchyk@richards-mbp.home>
2020-04-02 17:52:16 +0200

    ändern des Startlayers

--- world/main.js
+++ world/main.js
@@ -1,4 +1,4 @@
-let startLayer = L.tileLayer.provider('OpenTopoMap');
+let startLayer = L.tileLayer.provider('Stamen.TonerLite');
 let map = L.map("map", {
     center: [0, 0],
     zoom: 2,
@@ -7,14 +7,14 @@ let map = L.map("map", {
     ]
 });
 L.control.layers({
-    "OpenTopoMap": startLayer,
+    "OpenTopoMap": L.tileLayer.provider('OpenTopoMap'),
     //"Stadia.AlidadeSmooth" : L.tileLayer.provider('OStadia.AlidadeSmooth'),
     //"Stadia.AlidadeSmoothDark" : L.tileLayer.provider('Stadia.AlidadeSmoothDark'),
     "Thunderforest.OpenCycleMap": L.tileLayer.provider('Thunderforest.OpenCycleMap'),
     "Thunderforest.SpinalMap": L.tileLayer.provider('Thunderforest.SpinalMap'),
     "CyclOSM": L.tileLayer.provider('CyclOSM'),
     "Stamen.Watercolor": L.tileLayer.provider('Stamen.Watercolor'),
-    "Stamen.TonerLite": L.tileLayer.provider('Stamen.TonerLite'),
+    "Stamen.TonerLite": startLayer,
     "OpenStreetMap.DE": L.tileLayer.provider('OpenStreetMap.DE'),
 }).addTo(map);
 
@@ -25,15 +25,15 @@ for (let i = 1; i < CONFIRMED.length; i++) {
     const row = CONFIRMED[i];
     console.log(row[2], row[3]);
     let lat = row[2];
-    let lng = row [3];
+    let lng = row[3];
     let reg = `${row[0]}${row[1]}`;
-    let val =row[row.length-1];
-   // let mrk =L.marker ([lat,lng]).addTo(map);
-   // mrk.bindPopup(`${reg}: ${val}`);
+    let val = row[row.length - 1];
+    // let mrk =L.marker ([lat,lng]).addTo(map);
+    // mrk.bindPopup(`${reg}: ${val}`);
     let s = 0.5;
-    let r = Math.sqrt(val*s/Math.PI)
-    
-    let circle = L.circleMarker([lat,lng], {
+    let r = Math.sqrt(val * s / Math.PI)
+
+    let circle = L.circleMarker([lat, lng], {
         radius: r
     }).addTo(map);
     circle.bindPopup(`${reg}: ${val}`);
