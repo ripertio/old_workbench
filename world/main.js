a0c389de1b8ba53e807831018c4297a5927af7ac
Richard Kempert <ritchyk@richards-mbp.home>
2020-04-02 17:03:38 +0200

    auskommentieren von stadia

--- world/main.js
+++ world/main.js
@@ -1,17 +1,17 @@
 let startLayer = L.tileLayer.provider('OpenTopoMap');
-let map = L.map ("map", {
-    center: [0,0],
+let map = L.map("map", {
+    center: [0, 0],
     zoom: 2,
     layers: [
         startLayer
     ]
 });
 L.control.layers({
-    "OpenTopoMap" : startLayer,
-    "Stadia.AlidadeSmooth" : L.tileLayer.provider('OStadia.AlidadeSmooth'),
-    "Stadia.AlidadeSmoothDark" : L.tileLayer.provider('Stadia.AlidadeSmoothDark'),
-    "Thunderforest.OpenCycleMap" : L.tileLayer.provider('Thunderforest.OpenCycleMap'),
-    "Thunderforest.SpinalMap" : L.tileLayer.provider('Thunderforest.SpinalMap'),
-    "CyclOSM" : L.tileLayer.provider('CyclOSM'),
-    "Stamen.Watercolor" : L.tileLayer.provider('Stamen.Watercolor'),
-}).addTo(map)
+    "OpenTopoMap": startLayer,
+    //"Stadia.AlidadeSmooth" : L.tileLayer.provider('OStadia.AlidadeSmooth'),
+    //"Stadia.AlidadeSmoothDark" : L.tileLayer.provider('Stadia.AlidadeSmoothDark'),
+    "Thunderforest.OpenCycleMap": L.tileLayer.provider('Thunderforest.OpenCycleMap'),
+    "Thunderforest.SpinalMap": L.tileLayer.provider('Thunderforest.SpinalMap'),
+    "CyclOSM": L.tileLayer.provider('CyclOSM'),
+    "Stamen.Watercolor": L.tileLayer.provider('Stamen.Watercolor'),
+}).addTo(map);
