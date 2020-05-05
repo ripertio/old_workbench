4f2810f87f3b5baf7fd63d2f6bb80dd1a1304300
Richard Kempert <ritchyk@zid-vpns-15-162.uibk.ac.at>
2020-05-05 18:58:37 +0200

    hinzufügen der 16 und 15 einkehr koordinaten

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,15 @@
+const EINKEHR = [
+    [Etappennummer(18), HotelSchlossFernstein, 47.34563, 10.81764],
+    [Etappennummer(18), LoreaHuette, 47.34651, 10.79125],
+    [Etappennummer(18), HintereTarrentonalm, 47.32794, 10.72056],
+    [Etappennummer(18), AnhalterHuette, 47.30128, 10.66784],
+    [Etappennummer(17), Grubigalm, 47.38479, 10.86268],
+    [Etappennummer(17), HotelSchlossFernstein, 47.34563, 10.81764],
+    [Etappennummer(17), LoreaHuette, 47.34651, 10.79125],
+
+    [Etappennummer(16), Gaistalalm, 47.36667, 11.05],
+    [Etappennummer(16), Tillfussalm, 47.38333, 11.03333],
+    [Etappennummer(16), EhrwalderAlm, 47.38333, 10.96667],
+    [Etappennummer(15), Solsteinhaus, 47.31667, 11.28333],
+    [Etappennummer(15), EppzirlerAlm, 47.331042, 11.267198],
+];
